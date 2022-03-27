8897103725ccc43430eb4b74808a0169d9172507
Mirjam Kirchner <riedel.ks@gmail.com>
2022-03-27 18:54:10 +0200

    Implemented Ada with gradient descent

--- ch02/src/plotting.py
+++ ch02/src/plotting.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 from BinaryClassPerceptron import BinaryClassPerceptron
+from AdalinePerceptron import AdalinePerceptron
 from config import configure_plots, PATH_TO_FIGURES
 configure_plots()
 
@@ -30,6 +31,37 @@ def plot_number_of_updates(bcp: BinaryClassPerceptron, save_fig=True):
     return g
 
 
+def plot_comparison_of_adaline_learning_rates(X: np.array, y: np.array, etas=None, n_iter=20, save_fig=True):
+    if etas is None:
+        etas = [0.0001, 0.005, 0.1]
+
+    # Train Adaline perceptrons with different learning rates
+    ap_small_eta = AdalinePerceptron(eta=etas[0], n_iter=n_iter)
+    ap_small_eta.fit(X, y)
+    ap_medium_eta = AdalinePerceptron(eta=etas[1], n_iter=n_iter)
+    ap_medium_eta.fit(X, y)
+    ap_large_eta = AdalinePerceptron(eta=etas[2], n_iter=n_iter)
+    ap_large_eta.fit(X, y)
+
+
+    # Comparison of learning rates
+    fig, axes = plt.subplots(1, 3)
+    sns.lineplot(x=range(1, ap_small_eta.cost_.shape[0] + 1), y=ap_small_eta.cost_, ax=axes[0])
+    axes[0].set_title("Learning rate = " + str(etas[0]))
+    sns.lineplot(x=range(1, ap_medium_eta.cost_.shape[0] + 1), y=ap_medium_eta.cost_, ax=axes[1])
+    axes[1].set_title("Learning rate = " + str(etas[1]))
+    sns.lineplot(x=range(1, ap_large_eta.cost_.shape[0] + 1), y=ap_large_eta.cost_, ax=axes[2])
+    axes[2].set_title("Learning rate = " + str(etas[2]))
+    axes[2].set_yscale("log")
+    fig.suptitle("A too small learning rate requires a large number of epochs to converge to the global minimum cost.\n A too large learning rate overshoots the global minimum cost.")
+
+    if save_fig:
+        plt.savefig(os.path.join(PATH_TO_FIGURES, "adaline_perceptron-comparison-learning-rates.png"), transparent=True,
+                    bbox_inches="tight")
+
+    plt.show()
+
+
 def plot_decision_surface(X: np.array, y: np.array,  classifier, resolution=0.02, save_fig=True, xlabel="x",
                           ylabel="y"):
     x1_min, x1_max = X[:, 0].min()-1, X[:, 0].max()+1
@@ -45,8 +77,8 @@ def plot_decision_surface(X: np.array, y: np.array,  classifier, resolution=0.02
     g.set(title="Decision surface of the " + str(type(classifier).__name__) + "-classifier", xlabel=xlabel,
           ylabel=ylabel)
     if save_fig:
-        plt.savefig(os.path.join(PATH_TO_FIGURES, "binary_perceptron-decision-surface.png"), transparent=True,
-                    bbox_inches="tight")
+        plt.savefig(os.path.join(PATH_TO_FIGURES, str(type(classifier).__name__) + "-decision-surface.png"),
+                    transparent=True, bbox_inches="tight")
     plt.show()
     return g
 
